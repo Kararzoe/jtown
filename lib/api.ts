528291ef28035e49d89ad452c638c20e3d490a61
Jos Marketplace <jos@marketplace.com>
2026-06-08 12:05:01 +0100

    Switch backend URL to Hostinger

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://jos-backend.onrender.com/api';
+const API_URL = 'https://josmkt-com-ng-335845.hostingersite.com/api';
 
 const getToken = () => {
   if (typeof window !== 'undefined') {
