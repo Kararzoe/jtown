e5f82d14254f7f2fb77099837be04afff465209e
Jos Marketplace <jos@marketplace.com>
2026-09-14 11:28:36 +0100

    fix: set JosMKT logo as favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/josmkt-logo.png' }
+      ]
+    }
+  },
+
   compatibilityDate: '2025-01-01',
 
   ssr: false,
